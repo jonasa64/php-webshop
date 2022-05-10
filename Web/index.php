2a69648a920f98f04ec821ad45b12f0f70719b2a
jonasa64 <hvidovrefifa@gmail.com>
2022-05-10 22:28:50 +0200

    Require autolad file

--- Web/index.php
+++ Web/index.php
@@ -2,6 +2,8 @@
 
 namespace PHPSHOP;
 
+require(dirname(dirname(__FILE__)) . "/vendor/autoload.php");
+
 define('WEB', str_replace("Web/index.php", "",  $_SERVER["SCRIPT_NAME"]));
 define('ROOT', str_replace("Web/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
 
@@ -9,6 +11,7 @@ define('ROOT', str_replace("Web/index.php", "", $_SERVER["SCRIPT_FILENAME"]));
 require(ROOT . 'Router.php');
 require(ROOT. 'Request.php');
 require(ROOT. 'Dispatcher.php');
-
+require(ROOT . 'DB/DB.php');
+require(ROOT. 'Connection.php');
 $dispatch = new Dispatcher();
 $dispatch->dispatch();

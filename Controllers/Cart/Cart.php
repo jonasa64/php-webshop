182e231336e13c26091bb83449f2ffa307dab283
jonasa64 <hvidovrefifa@gmail.com>
2022-05-13 23:09:29 +0200

    Cart controller

---
+++ Controllers/Cart/Cart.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PHPSHOP\Controllers\Cart;
+
+use \PHPSHOP\Controllers\Controller;
+
+class Cart extends Controller{
+
+
+public function __construct()
+{
+    parent::__construct();
+}
+
+public function index(){
+
+}
+
+public function add(){
+
+}
+
+public function remove(){
+
+}
+
+public function clear(){
+    
+}
+
+public function update(){
+
+}
+
+}

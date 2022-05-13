54be06c821481f41d20bef1321c7478afa799fd8
jonasa64 <hvidovrefifa@gmail.com>
2022-05-13 22:28:29 +0200

    Products view

--- views/products.php
+++ views/products.php
@@ -1,40 +1,34 @@
 <?php
-$title = "products";
-include_once dirname(dirname(__FILE__)) . "/shared/header.php";?>
+$title = "Products";
+include_once dirname(dirname(__FILE__)) . "/shared/header.php";
+?>
 
 <div class="container mt-4">
-<div class="row">
-    <?php
-$query = "SELECT * FROM products";
-$stmt = mysqli_query($connection, $query);
-
-while ($rows = mysqli_fetch_assoc($stmt)){
-    $id = $rows['id'];
-    $name = $rows['name'];
-    $price = $rows['price'];
-    $description = $rows['description'];
-    $image = $rows['image_url'];
- ?>
-
-    <div class="col-4">
-        <?php echo "<p>$name</p>"?>
-        <img src="<?php echo $image?>" alt="<?php echo $name?>">
-        <?php echo "<p>$price</p>"?>
-        <?php echo  "<p>$description</p>"?>
-        <form method="post" action="cart.php">
+    <div class="row">
+        <?php foreach($data as $product): ?>
+            <div class="col-4">
+                <p><?=$product->name?></p>
+                <img src="<?=$product->image?>" alt="<?= $product->name?>"/>
+                <p><?=$product->description?></p>
+                <p><?=$product->status?></p>
+                <p><?=$product->price?></p>
+                <form method="post" action="cart.php">
+                    <?php if($product->quantityInStock > 0) : ?>
             <div class="form-group">
                 <label for="quantity">Quantity</label>
                 <input class="form-control" id="quantity" type="text" name="quantity" value="1"/>
             </div>
-            <input type="hidden" name="id" value="<?php echo $id;?>"/>
-            <input type="hidden" name="name" value="<?php echo $name;?>"/>
-            <input type="hidden" name="price" value="<?php echo $price;?>"/>
+            <input type="hidden" name="id" value="<?php $product->id;?>"/>
+            <input type="hidden" name="name" value="<?= $product->name;?>"/>
+            <input type="hidden" name="price" value="<?= $product->price;?>"/>
             <input class="btn btn-primary" type="submit" name="add_to_cart" value="Add to card"/>
         </form>
+<?php endif; ?>
+            </div>
+            <?php endforeach; ?>
     </div>
-<?php }
 
-include_once dirname(dirname(__FILE__)) . "/shared/footer.php";
-?>
+</div>
+
 
 

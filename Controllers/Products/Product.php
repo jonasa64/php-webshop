55e1a869d49ab6b7ade8346b9ad1e0cb41d67c59
jonasa64 <hvidovrefifa@gmail.com>
2022-05-13 15:37:07 +0200

    Some SQL stmt

--- Controllers/Products/Product.php
+++ Controllers/Products/Product.php
@@ -1,19 +1,30 @@
 <?php 
 
-namespace PHPSHOP\Controllers\Product;
+namespace PHPSHOP\Controllers\Products;
 
-class Product {
+use PHPSHOP\Controllers\Controller;
+
+class Product extends Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
     
     public function index() {
 
+        $sql = "SELECT * FROM products";
+        $result = \PHPSHOP\DB\DB::query($sql);
+        print_r($result->fetch_assoc());
     }
 
-    public function show(){
+    public function show(int $id){
+        $sql = "SELECT * FROM products WHERE id = ?";
 
     }
 
-    public function destroy(){
-
+    public function destroy(int $id){
+    $sql = "DELETE FROM products WHERE id = ?";
     }
 
     public function edit(){

bbb349f4a88f02513fc845d1f924eb2703636345
jonasa64 <hvidovrefifa@gmail.com>
2022-05-15 00:11:22 +0200

    Style changes

--- views/products.php
+++ views/products.php
@@ -7,23 +7,14 @@ include_once dirname(dirname(__FILE__)) . "/shared/header.php";
     <div class="row">
         <?php foreach($data as $product): ?>
             <div class="col-4">
-                <p><?=$product->name?></p>
-                <img src="<?=$product->image?>" alt="<?= $product->name?>"/>
-                <p><?=$product->description?></p>
-                <p><?=$product->status?></p>
-                <p><?=$product->price?></p>
-                <form method="post" action="cart.php">
-                    <?php if($product->quantityInStock > 0) : ?>
-            <div class="form-group">
-                <label for="quantity">Quantity</label>
-                <input class="form-control" id="quantity" type="text" name="quantity" value="1"/>
-            </div>
-            <input type="hidden" name="id" value="<?php $product->id;?>"/>
-            <input type="hidden" name="name" value="<?= $product->name;?>"/>
-            <input type="hidden" name="price" value="<?= $product->price;?>"/>
-            <input class="btn btn-primary" type="submit" name="add_to_cart" value="Add to card"/>
-        </form>
-<?php endif; ?>
+                <div class="card">
+                <img class="card-img-top mt-3" src="<?=$product->image?>" alt="<?= $product->name?>"/>
+                <div class="card-body">
+                <h5 class="card-title"><?=$product->name?></h5>
+                <a href="#" class="btn btn-primary">Show</a>
+                </div>
+
+                </div>
             </div>
             <?php endforeach; ?>
     </div>

7e0ef6fdbf6b3b63f9dc0ed256310c270a5b78e4
jonasa64 <hvidovrefifa@gmail.com>
2022-05-15 00:13:16 +0200

    Updated bootstrap version

--- shared/header.php
+++ shared/header.php
@@ -1,15 +1,13 @@
 <?php
 session_start();
 ob_start();
-include_once  dirname(dirname(__FILE__)) . "/Connection.php";
-include_once dirname(dirname(__FILE__))  . "/shared/functions.php";
 ?>
 
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <link rel="stylesheet" href="http://localhost:81/phpShop/shared/style.css" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <meta charset="UTF-8">
         <title><?php echo $title?></title>
     </head>
@@ -27,9 +25,5 @@ include_once dirname(dirname(__FILE__))  . "/shared/functions.php";
     }
 ?>
 
-    <?php
-    $conn = new Connection();
-   $connection = $conn->getConnection();
-    ?>
 <?php include_once dirname(dirname(__FILE__)). "/shared/nav.php"; ?>
 

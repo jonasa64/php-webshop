518f7ef80e4a17a13e498fece187ab85f8341293
jonasa64 <hvidovrefifa@gmail.com>
2022-05-17 23:08:05 +0200

    changes

--- DB/DB.php
+++ DB/DB.php
@@ -1,110 +1,124 @@
-<?php 
+<?php
 
 namespace PHPSHOP\DB;
 
-use Connection;
 
-class DB{
+class DB
+{
 
 
-private static function getConnect(){
-    $connection = new \PHPSHOP\Connection;
-    return $connection->getConnection();
-}
+    private static function getConnect()
+    {
+        $connection = new \PHPSHOP\Connection;
+        return $connection->getConnection();
+    }
 
     /**
      * Insert a new DB record
      *
      * @param string $tableName
-     * @param array $data
+     * @param array  $data      
+     * 
      * @return bool
      */
-    public static function insert($tableName, $data){
+    public static function insert($tableName, $data)
+    {
 
         // Check that data is array
-        if(!is_array($data)) return false;
+        if (!is_array($data)) return false;
 
         // CHeck that that the length of data is greather than 0
-        if(is_array($data) && count($data) == 0) return false;
+        if (is_array($data) && count($data) == 0) return false;
 
         // Check if table name is not set or empty 
-        if(!isset($tableName) || empty($tableName)) return false;
+        if (!isset($tableName) || empty($tableName)) return false;
 
         $keys = array_keys($data);
-        
+
         $columenNames = implode(",", $keys);
 
         $sql_stmt = "INSERT INTO " . $tableName . "(" . $columenNames . ") values(";
 
         $sqlValues = array_values($data);
 
-        foreach($sqlValues as $value){
-            if($value == $sqlValues[count($sqlValues) -1])
+        foreach ($sqlValues as $value) {
+            if ($value == $sqlValues[count($sqlValues) - 1])
                 $sql_stmt .= "?)";
-            else 
-            $sql_stmt .= "?,";
+            else
+                $sql_stmt .= "?,";
         }
 
         self::prepare($sql_stmt);
-
     }
 
     /**
      * Insert many DB record 
      *
      * @param string $tableName
-     * @param array $data
+     * @param array  $data     
+     * 
      * @return bool
      */
-    public static function insertMany($tableName, $data){
-
+    public static function insertMany($tableName, $data)
+    {
     }
 
-    public static function update($tableName, $data, $whereSQL, $whereValues = []){
 
+    /**
+     * Undocumented function
+     *
+     * @param string $tableName  
+     * @param array  $data        
+     * @param array  $whereSQL    
+     * @param array  $whereValues 
+     * 
+     * @return void
+     */
+    public static function update($tableName, $data, $whereSQL, $whereValues = [])
+    {
     }
 
     /**
      * Delete record from DB
      *
-     * @param string $tableName
-     * @param int $id
+     * @param string $tableName 
+     * @param int    $id        
+     * 
      * @return bool
      */
-    public static function delete($tableName, $id){
+    public static function delete($tableName, $id)
+    {
 
         $sql = "DELETE FROM " . $tableName . " WHERE id = ?";
         $query =  self::prepare($sql);
         $query->bind_param("i", $id);
         $query->execute();
         return $query->affected_rows > 0;
-
     }
 
-    private static function bind($value){
-
-        switch($value){
-
-        }
-        
-    }
 
     /**
      * Prepare SQL statement
      *
-     * @param string $sql
+     * @param string $sql 
+     * 
      * @return \mysqli_stmt|false
      */
-    public static function prepare($sql){
-         $query = self::getConnect()->prepare($sql);
-         return $query;
-        
+    public static function prepare($sql)
+    {
+        $query = self::getConnect()->prepare($sql);
+        return $query;
     }
-
-    public static function query($sql){
+    /**
+     * Undocumented function
+     *
+     * @param string $sql 
+     * 
+     * @return void
+     */
+    public static function query($sql)
+    {
         $query = self::getConnect()->query($sql);
         return $query;
     }
-
-
 }

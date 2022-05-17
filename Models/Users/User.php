4471bbda55aff09dcf434cc50571074c9aabfeee
jonasa64 <hvidovrefifa@gmail.com>
2022-05-17 21:13:28 +0200

    Logic to more than one user

--- Models/Users/User.php
+++ Models/Users/User.php
@@ -25,6 +25,43 @@ class User {
 
     }
 
+    public function get($identifiers){
+
+        // Check if identifiers is array with a length of 0 
+        if(is_array($identifiers) && count($identifiers) == 0) return null;
+
+        if(!is_array($identifiers) && empty($identifiers)) return null;
+
+        // Check if identifers is not array and not int
+        if(!is_array($identifiers) && !is_int($identifiers)) return null;
+
+        if(is_array($identifiers) && count($identifiers) > 0){
+            $users = [];
+
+            $sql = "SELECT id, first_name, last_name, email, is_admin FROM users WHERE id IN(". implode(",", $identifiers) . ")";
+            $query = \PHPSHOP\DB\DB::query($sql);
+            
+            while($row = $query->fetch_assoc()){
+                $this->id = $row["id"];
+                $this->firstName = $row["first_name"];
+                $this->lastName = $row["last_name"];
+                $this->email = $row["email"];
+                $this->isAdmin = $row["is_admin"];
+                $users[] = $this;
+            }
+
+            return $users;
+        }
+
+        if(!is_array($identifiers) && is_int($identifiers)){
+
+            return $this;
+        }
+
+        return null;
+
+    }
+
     private function doesUsersExist(string $email) {
     }
 
